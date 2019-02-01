5c23c8fbdd7e89179cc2ebec48ccfd57391d09f5
Galina Is <denis@isyanov.com>
2019-02-01 18:02:04 +0300

    Выполнен п.2 к ДЗ урока 1

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'mysql:host=localhost;dbname=task_adv',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
